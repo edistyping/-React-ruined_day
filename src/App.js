0b024abf3d54a36f0f159d58b860d98029dc1048
themadman <edwardnkim.93@gmail.com>
2019-11-26 22:43:27 -0800

    Working to add InfiniteLoader

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Fade from 'react-reveal/Fade';
-import Pulse from 'react-reveal/Pulse';
+import Pulse from 'react-reveal/Pulse'
+
+import { InfiniteLoader, List } from 'react-virtualized';
+import 'react-virtualized/styles.css'; // only needs to be imported once
 
 import fire from "./fire";
 import firebase from 'firebase';
@@ -13,11 +16,8 @@ class App extends Component {
     super(props);
     this.state = { counter: 0, reasons: [], reason_provided: 0, isHovering: false};
 
-    this.optionText = this.optionText.bind(this); // added
-    
-    this.hoverEnter = this.hoverEnter.bind(this);
-    this.hoverLeave = this.hoverLeave.bind(this);
-    this.hoverButton = this.hoverButton.bind(this);
+    this.optionText = this.optionText.bind(this); // testing phase
+    this.hoverButton = this.hoverButton.bind(this); // working phase
   }
     
   componentWillMount(){
@@ -26,10 +26,8 @@ class App extends Component {
     let responseRef = fire.database().ref('user_response').orderByKey().limitToLast(5);
     responseRef.on('child_added', snapshot => {
       let response = { reason: snapshot.val().opinion, id: snapshot.key, row_num: snapshot.val().id };   
-
       this.setState({ reasons: [response].concat(this.state.reasons), counter: (snapshot.val().id + 1) });
       console.log("componentwillmount() here... ");
-      // - This 'child_added' callback will be called throughout  afafaf
     })
 
     // Next, check if there are more than 100 posts. If so, delete the oldest. 
@@ -42,7 +40,6 @@ class App extends Component {
     // Fetch the data in the input textbox and load it the database with the inserted_time
     var inputValue = this.inputEl.value;
     fire.database().ref('user_response').push( {opinion: inputValue, response_time: firebase.database.ServerValue.TIMESTAMP, answer: 1, id: this.state.counter});
-
 
     // Reset the input textbox  
     this.inputEl.value = "";    
@@ -65,18 +62,6 @@ class App extends Component {
   }
 
   // Everytime an user hovers over the button, it does 'Pulse' animation.
-  hoverEnter(){
-      //e.preventDefault();
-      console.log("Hovered on...");
-      this.setState({isHovering: true}); 
-  }
-
-  hoverLeave(){
-      //e.preventDefault();
-      console.log("Hovered out...");
-      this.setState({isHovering: false}); 
-  }
-
   hoverButton(){
       //e.preventDefault();
       this.setState(prevState  => ({
@@ -86,7 +71,7 @@ class App extends Component {
 
 
   render() {
-    const btnClass = this.state.isHovering ? "pulse animated" : "";
+    const btnClass = this.state.isHovering ? "pulse animated" : ""; // we don't need this since 'spy' 
 
     return (
     
@@ -95,18 +80,24 @@ class App extends Component {
         <h1>Is your Day Ruined?</h1>
         </Fade>
         
-        
-        <Pulse spy={this.state.isHovering}>   
+        <Pulse spy={this.state.isHovering} >   
           <input type="submit" className={btnClass}  value="Yes, it is" onMouseEnter={this.hoverButton} onMouseLeave={this.hoverButton} />
         </Pulse>
         <input type="text" ref={ el => this.inputEl = el } onClick={this.optionText}/>
         
-        <p>Number of ruined people today and their reasons below: {this.state.counter}</p>        
-        <ul>
-        {
-          this.state.reasons.map( response => <li key={response.id}>{response.reason}</li> )
-        }
-        </ul>
+        <div>
+          <p>Number of ruined people today and their reasons below: {this.state.counter}</p>        
+          <ul>
+          {
+            this.state.reasons.map( response => <li key={response.id}>{response.reason}</li> )
+          }
+          </ul>
+        </div>
+
+        <InfiniteLoader>
+
+        </InfiniteLoader>
+
       </form>
 
 
