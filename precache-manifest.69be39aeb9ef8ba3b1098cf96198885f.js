e7d5471e8776fda7938a439fd45641e9c4047aa3
themadman <edwardnkim.93@gmail.com>
2019-12-04 09:21:58 -0800

    Updates

---
+++ precache-manifest.69be39aeb9ef8ba3b1098cf96198885f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e540726875f796cef9632640877ddf8",
+    "url": "/ruined_day/index.html"
+  },
+  {
+    "revision": "48d74eed63b4bdb86ef3",
+    "url": "/ruined_day/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "7dfdd28d0f10d9393c1e",
+    "url": "/ruined_day/static/css/main.fb9c2bbb.chunk.css"
+  },
+  {
+    "revision": "48d74eed63b4bdb86ef3",
+    "url": "/ruined_day/static/js/2.e37b644b.chunk.js"
+  },
+  {
+    "revision": "7dfdd28d0f10d9393c1e",
+    "url": "/ruined_day/static/js/main.f701c2b0.chunk.js"
+  },
+  {
+    "revision": "9ac64c41b1867e53a303",
+    "url": "/ruined_day/static/js/runtime-main.83040e68.js"
+  }
+]);
